26ac253326a1153ffaeeb78e4c09f8276fc6b89c
Yudha Aris Kristanto <yudhaaris898@gmail.com>
2025-10-17 21:18:29 +0700

    memperbaiki case sensitive di yang sebelumnya Linux menjadi Windows

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -9,7 +9,7 @@ class MY_Controller extends CI_Controller
     {
         parent::__construct();
         $model = strtolower(get_class($this));
-        if (file_exists(APPPATH . 'models/' . $model . '_model.php')) {
+        if (file_exists(APPPATH . 'models/' . ucfirst($model) . '_model.php')) {
             $this->load->model($model . '_model', $model, true);
     }
     }
